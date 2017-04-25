ba4e6be74dd75df7bbcdb63fc41b38a9155a97cd
Maciej Szlosarczyk <maciej.szlosarczyk@inbank.ee>
2017-04-25 16:20:23 +0300

    Update version to 3.1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HTTPotion.Mixfile do
     [ app: :httpotion,
       name: "httpotion",
       source_url: "https://github.com/myfreeweb/httpotion",
-      version: "3.1.2",
+      version: "3.1.0",
       elixir:  "~> 1.1",
       docs: [ extras: ["README.md", "CODE_OF_CONDUCT.md"] ],
       description: description(),
